0c1d114c977a45eb48506838e3814d21a9cc1836
Neha SR <nehasr2214@gmail.com>
2025-09-17 15:38:20 +0530

    initial commit

---
+++ quiz.py
@@ -0,0 +1,153 @@
+print("Welcome to the game!")
+
+playing=input("Do you want to play this game ? ")
+
+if playing.lower()!= "yes":
+   
+   quit()
+
+print("OK lets play!")
+
+score=0
+
+answer=input("which planet is known as red planet? ")
+
+if answer.lower()== ("mars"):
+
+  print("Congrats correct answer!")
+
+  score+=1
+
+else:
+
+    print("Wrong Answer!")
+
+answer=input("What is the largest Ocean in the World? ")
+
+if answer.lower()== ("pacific ocean"):
+ 
+  print("Congrats correct answer!")
+
+  score +=1
+
+else:
+  
+  print("Wrong Answer!")
+
+answer=input("Who Invented Telephone? ")
+
+if answer.lower()== ("alexander graham bell"):
+
+  print("Congrats correct answer!")
+
+  score +=1
+
+else:
+  
+  print("Wrong Answer!")
+
+answer=input("what is the capital of France? ")
+
+if answer.lower()== ("paris"):
+  
+  print("Congrats correct answer!")
+
+  score +=1
+
+else:
+  
+ print("Wrong Answer!")  
+
+answer=input("Which Country is known as land of Rising Sun? ")
+
+if answer.lower()== ("japan"):
+  
+  print("Congrats correct answer!")
+
+  score +=1
+
+else:
+  
+ print("Wrong Answer!")  
+
+answer=input("Which is the only Continent without a desert? ")
+
+if answer.lower()== ("europe"):
+  
+  print("Congrats correct answer!")
+
+  score +=1
+
+else:
+  
+ print("Wrong Answer!")  
+
+answer=input("Which year India gain Independence? ")
+
+if answer== str(1947):
+  
+  print("Congrats correct answer!")
+
+  score +=1
+
+else:
+  
+ print("Wrong Answer!")  
+
+answer=input("Center of an atom is called? ")
+
+if answer.lower()== ("neutron"):
+  
+  print("Congrats correct answer!")
+
+  score +=1
+
+else:
+  
+ print("Wrong Answer!")  
+
+answer=input("Who wrote 'Romeo and Juliet? ")
+
+if answer.lower()== ("william shakespeare"):
+  
+  print("Congrats correct answer!")
+
+  score +=1
+
+else:
+  
+  print("Wrong Answer!")  
+
+answer=input("Who discovered America? ")
+
+if answer.lower()== ("christopher columbus"):
+  
+  print("Congrats correct answer!")
+
+  score +=1
+
+else:
+  
+ print("Wrong Answer!") 
+
+print("Bravo!! you have completed the Quiz")
+
+print("Your Score is :",score )
+
+  
+
+
+
+
+
+  
+
+  
+  
+
+
+
+  
+
+
+
